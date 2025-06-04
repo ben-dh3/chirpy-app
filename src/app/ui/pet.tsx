12694e6678838afdaaaa116484335eaea9ff3f12
ben-dh3 <benjaminduffield1997@gmail.com>
2025-06-04 13:35:55 +0100

    update evolve button

--- src/app/ui/pet.tsx
+++ src/app/ui/pet.tsx
@@ -37,7 +37,7 @@ export default async function Pet({ userId }: { userId: string }) {
       <Timer userId={userId} dailyGoal={getDailyGoal(petData.stage)} />
       {canEvolve(petData.stage, petData.total_minutes) && (
         <form className="flex items-center justify-center" action={evolvePet.bind(null, userId)}>
-          <button type="submit" className="cursor-pointer w-full bg-starfield text-white p-2 rounded-2xl">
+          <button type="submit" className="cursor-pointer w-full bg-secondary-500 drop-shadow-evolve text-white p-2 rounded-2xl">
             Evolve
           </button>
         </form>
