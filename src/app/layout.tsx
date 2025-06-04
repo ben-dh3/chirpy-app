8cec0131e6e1c5e63402e427fdb83305f87fad02
ben-dh3 <benjaminduffield1997@gmail.com>
2025-06-04 13:40:13 +0100

    add favicon reference

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+      </head>
       <body
         className={`${sen.className} antialiased p-12`}
       >
